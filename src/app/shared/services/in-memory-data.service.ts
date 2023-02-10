e9316311c6bd782853825d37719d5b806ef064a7
corrigentia <ecasuta@gmail.com>
2023-02-10 13:59:51 +0100

    attempt mock db expansion

--- src/app/shared/services/in-memory-data.service.ts
+++ src/app/shared/services/in-memory-data.service.ts
@@ -1,42 +1,76 @@
-import { Injectable } from '@angular/core';
-import { InMemoryDbService } from 'angular-in-memory-web-api';
-import { StudentIdEmail } from '../../interfaces/student-id-email';
+import { Injectable } from '@angular/core'
+import { InMemoryDbService } from 'angular-in-memory-web-api'
+import { StudentIdEmail } from '../../interfaces/student-id-email'
 // import { Student } from '../interfaces/student';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class InMemoryDataService implements InMemoryDbService {
-  createDb() {
+  createDb () {
     const students = [
       {
         studentId: 12,
-        email: 'masutatsu@oyama.kr',
+        email: 'masutatsu@oyama.kr'
       },
       {
         studentId: 13,
-        email: 'gichin@funakoshi.jp',
+        email: 'gichin@funakoshi.jp'
       },
       {
         studentId: 14,
-        email: 'anko@itosu.jp',
+        email: 'anko@itosu.jp'
       },
       {
         studentId: 15,
-        email: 'chouki@motobu.jp',
+        email: 'chouki@motobu.jp'
+      }
+    ]
+
+    const equipments = [
+      {
+        equipmentId: 1,
+        name: 'Bo stick',
+        price: 129.98
       },
-    ];
+      {
+        equipmentId: 2,
+        name: 'Bokken',
+        price: 22.0
+      },
+      {
+        equipmentId: 3,
+        name: 'Hanbo',
+        price: 16.9
+      },
+      {
+        equipmentId: 4,
+        name: 'Katana',
+        price: 169.0
+      },
+      {
+        equipmentId: 5,
+        name: 'Tanto',
+        price: 90.0
+      },
+      {
+        equipmentId: 6,
+        name: 'Uniform',
+        price: 117.5
+      }
+    ]
 
     // TODO: discover reason why this must be encapsulated...!!
     // return students;
-    return { students };
+    return { equipments, students }
+    return { students, equipments }
   }
 
-  constructor() {}
+  constructor () {}
 
-  genId(students: StudentIdEmail[]): number {
+  genId (students: StudentIdEmail[]): number {
     return students.length > 0
-      ? Math.max(...students.map((student) => student.studentId)) + 1
-      : 11;
+      ? Math.max(...students.map(student => student.studentId)) + 1
+      : 11
   }
 }
